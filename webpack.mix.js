ae1af9c2feecf3cdf2dc779610cbd4ab2012aafa
TheKinng96 <fengyuan.yap@example.com>
2020-09-24 20:14:22 +0900

    added simple json database for testing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -10,8 +10,12 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.browserSync("127.0.0.1:8000");
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [
         //
-    ]);
+    ]
+);
