c7bee702bead8154b6b7592a3372c5e2f4ebab26
TheKinng96 <fengyuan.yap@example.com>
2020-09-25 19:55:35 +0900

    added subject api call functions

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SclassController;
+use App\Http\Controllers\Api\SubjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use App\Http\Controllers\Api\SclassController;
 */
 
 Route::resource('/class', SclassController::class);
+Route::resource('/subject', SubjectController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
